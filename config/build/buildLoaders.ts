86b3c6bba108a56bc4cddec048fda3fe29b72746
SKaterinenko <skaterinenko@gmail.com>
2024-04-06 17:41:28 +0500

    Update webpack config

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,36 @@
 import webpack from "webpack";
+import {BuildOptions} from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export const buildLoaders = (): webpack.RuleSetRule[] => {
+export const buildLoaders = ({isDev}: BuildOptions): webpack.RuleSetRule[] => {
 
     const typeScriptLoader = {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }
-        return [typeScriptLoader]
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]",
+                    },
+                },
+            },
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    }
+
+    return [
+        typeScriptLoader,
+        cssLoader
+    ]
 }
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-export function someFn(arg: string): string {
-    console.log(`Random function 343 ${arg}`)
-
-    return "ewe"
-}
