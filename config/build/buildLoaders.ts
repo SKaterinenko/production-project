340ba02650a26ad6da440620bdc78aefde31d335
SKaterinenko <skaterinenko@gmail.com>
2024-04-07 20:35:42 +0500

    Update project

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,36 +1,37 @@
 import webpack from "webpack";
-import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export const buildLoaders = ({isDev}: BuildOptions): webpack.RuleSetRule[] => {
-
-    const typeScriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
-            // Creates `style` nodes from JS strings
-            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
             {
                 loader: "css-loader",
                 options: {
                     modules: {
                         auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]",
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]'
                     },
-                },
+                }
             },
-            // Compiles Sass to CSS
             "sass-loader",
         ],
     }
 
+    // Если не используем тайпскрипт - нужен babel-loader
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
     return [
-        typeScriptLoader,
-        cssLoader
+        typescriptLoader,
+        cssLoader,
     ]
 }
--- config/build/buildResolves.ts
+++
@@ -1,7 +0,0 @@
-import webpack from "webpack";
-
-export const buildResolves = (): webpack.ResolveOptions => {
-    return {
-        extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
-    }
-}
